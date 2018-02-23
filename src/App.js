9367be05b5332ef1cc17181e516549449b6cdc47
Savannah Cody <savannah.m.cody@gmail.com>
2018-02-23 12:57:51 -0500

    worked on improving API

--- src/App.js
+++ src/App.js
@@ -19,13 +19,14 @@ class Doctor extends Component {
   handleSubmit = (e) => {
   // handleSubmit (e) {
     // if (e.keyCode == 13 || onClick) {
-    console.log(e)
+    console.log('event is', e)
     e.preventDefault()
-    axios.get("https://api.betterdoctor.com/2016-03-01/practices?location=38.9048626%2C-77.0340224%2C50&user_location=38.904%2C-77.034&sort=distance-asc&fields=location_slug%2C%20within_search_area%2C%20distance%2C%20uid%2C%20npi%2C%20name%2C%20website%2C%20description%2C%20accepts_new_patients%2C%20visit_address%2C%20office_hours%2C%20phones%2C%20languages&skip=0&limit=100&user_key=1b5cb38b9322c2b77c59620b1dd17ed4")
+    axios.get("https://api.betterdoctor.com/2016-03-01/practices?location=37.773%2C-122.413%2C100&user_location=38.904%2C%20-77.034&sort=distance-asc&fields=name%2C%20visit_address(StreetAddress)%2C%20distance%2C%20slug%2C%20office_hours%2C%20website%2C%20description%2C%20phone_number(number%2C%20type)%2C%20rating%2C%20Specialty(name%2C%20description%2C%20category)&skip=0&limit=15&user_key=1b5cb38b9322c2b77c59620b1dd17ed4")
     .then(response => {
       this.setState({doctors: response.data.data})
       console.log('response data is ', response.data)
     })
+    // .catch(err => console.log('from handleSubmit, err is', err))
   // }
 }
 
@@ -34,19 +35,21 @@ class Doctor extends Component {
       <div className="container">
         <div className="Title">
           <div className="bigTitle">  
-            <h1>Holistic Doctor</h1>
+            <marquee><h1>Holistic Doctor</h1></marquee>
           </div>
             <h3>near me</h3>
           </div>
-        <div className="zipcode">
-        <div className="row">
-          <input type="text" name="zip-code" placeholder="Zip Code"/> 
-        </div>
+
+          <div className="row">
+            <div className="input-field col s5 offset-s2">
+              <input className="zipcode" type="text" name="zip-code" placeholder="Zip Code"/> 
+            </div>
+
+            <div className="button black btn waves-effect waves-red">
+              <button onClick={this.handleSubmit}>Check Near Me </button>
+            </div>
         </div>
 
-        <div className="button">
-          <button onClick={this.handleSubmit}>Check Near Me</button>
-        </div>
         {/* <input type="text" name="zip-code" placeholder="Zip Code"/> <button type="submit" onSubmit={(e) => this.onHandleSubmit(e)}>Check Near Me</button> */}
       
         {console.log('doctors from render is', this.state.doctors)}
@@ -54,9 +57,13 @@ class Doctor extends Component {
         {
         this.state.doctors.map((pract, index) => {
           return <div className="results">
-          <li key={index}>{pract.name} <br/> Street: {pract.visit_address.street} <br/> City: {pract.visit_address.city} <br/> Zip Code: {pract.visit_address.zip} <br/> </li></div>
+                  <li key={index}>{pract.name} <br/> 
+                  Street: {pract.practice} <br/> 
+                  City: {pract.visit_address.city} State: {pract.visit_address.state} <br/> 
+                  Zip Code: {pract.visit_address.zip} <br/> </li>
+                </div>
           console.log(pract)
-        })
+        })   
       }
       </div>
     )
